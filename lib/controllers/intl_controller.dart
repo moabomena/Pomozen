2ce54b38930049d59fd961c6c23ca8fc8620b20b
Moab Omena <1moabomena@gmail.com>
2022-06-24 01:11:40 -0300

    create a new controller to localizations or intl

--- lib/controllers/intl_controller.dart
+++ lib/controllers/intl_controller.dart
@@ -1,6 +1,27 @@
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 
 ValueNotifier<String> chosenValueLanguage = ValueNotifier('English');
+ValueNotifier<String> intl = ValueNotifier('en');
 
-void setChosenValueLanguage(var newValue) =>
-    chosenValueLanguage.value = newValue;
+void setChosenValueLanguage(var newValue) {
+  chosenValueLanguage.value = newValue;
+}
+
+void setIntl(var newValue) {
+  intl.value = newValue;
+}
+
+choseIntl() {
+  switch (chosenValueLanguage.value) {
+    case 'English':
+      setIntl('en');
+      break;
+    case 'Spanish':
+      setIntl('es');
+      break;
+    case 'Portuguese':
+      setIntl('pt');
+      break;
+    default:
+  }
+}

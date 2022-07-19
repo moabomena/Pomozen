29228c3123c6a7d91720e66434ee96db8ca1052d
Moab Omena <1moabomena@gmail.com>
2022-07-19 08:55:16 -0300

    add border rounded and spacing in list tile of drawer

--- lib/widgets/drawer/custom_listile.dart
+++ lib/widgets/drawer/custom_listile.dart
@@ -13,27 +13,34 @@ class CustomListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      tileColor: indexPage.value == indexSelect
-          ? Theme.of(context).listTileTheme.tileColor
-          : Colors.transparent,
-      leading: Icon(icon,
-          size: MediaQuery.of(context).size.width * 0.06,
-          color: indexPage.value == indexSelect
-              ? Colors.orange
-              : Theme.of(context).textTheme.bodyText1!.color),
-      minLeadingWidth: 1,
-      title: Text(
-        title!,
-        style: TextStyle(
+    return Padding(
+      padding: const EdgeInsets.only(right: 15, left: 15),
+      child: ListTile(
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(12))),
+        tileColor: indexPage.value == indexSelect
+            ? Theme.of(context).listTileTheme.tileColor
+            : Colors.transparent,
+        leading: Icon(icon,
+            size: MediaQuery.of(context).size.width * 0.06,
             color: indexPage.value == indexSelect
-                ? Colors.orange
-                : Theme.of(context).textTheme.bodyText1!.color,
-            fontSize: MediaQuery.of(context).size.width * 0.045,
-            fontWeight: FontWeight.w500,
-            fontFamily: 'OpenSans'),
+                ? Colors.white
+                : Theme.of(context).textTheme.bodyText1!.color),
+        minLeadingWidth: 1,
+        title: Text(
+          title!,
+          style: TextStyle(
+              color: indexPage.value == indexSelect
+                  ? Colors.white
+                  : Theme.of(context).textTheme.bodyText1!.color,
+              fontSize: MediaQuery.of(context).size.width * 0.045,
+              fontWeight: indexPage.value == indexSelect
+                  ? FontWeight.w700
+                  : FontWeight.w500,
+              fontFamily: 'OpenSans'),
+        ),
+        onTap: ontap,
       ),
-      onTap: ontap,
     );
   }
 }

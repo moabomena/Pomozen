4480561943d7371af78185bde0b42e6f71f2a03c
Moab Omena <1moabomena@gmail.com>
2022-07-22 10:38:11 -0300

    add url launcher in contact tile for link sites

--- lib/widgets/contact_tile.dart
+++ lib/widgets/contact_tile.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:pomodoro_app/controllers/theme_controller.dart';
+import 'package:pomodoro_app/responsive/dimensions.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ContactTile extends StatelessWidget {
   const ContactTile(
@@ -17,6 +19,7 @@ class ContactTile extends StatelessWidget {
       this.gradientColorQuaternary,
       this.isBackground = false,
       this.isContrast = false,
+      this.url,
       Key? key})
       : super(key: key);
 
@@ -33,96 +36,119 @@ class ContactTile extends StatelessWidget {
   final bool isGradientColorContainer;
   final bool isBackground;
   final bool isContrast;
+  final String? url;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(10),
-      margin: const EdgeInsets.only(bottom: 10, top: 10),
-      decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.primary,
-          borderRadius: BorderRadius.circular(12)),
-      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-        Row(
-          children: [
-            isContained
-                ? Container(
-                    padding: const EdgeInsets.all(8),
-                    height: 50,
-                    width: 50,
-                    decoration: BoxDecoration(
-                        gradient: isGradientColorContainer
-                            ? LinearGradient(
-                                begin: Alignment.topRight,
-                                end: Alignment.bottomLeft,
-                                stops: const [
-                                    0.1,
-                                    0.4,
-                                    0.6,
-                                    0.9,
-                                  ],
-                                colors: [
-                                    gradientColorPrimary!,
-                                    gradientColorSecondary!,
-                                    gradientColorTertiary!,
-                                    gradientColorQuaternary!,
-                                  ])
-                            : null,
-                        color: isGradientColorContainer ? null : colorContainer,
-                        shape: BoxShape.circle),
-                    child: SvgPicture.asset(
-                      pathSvg!,
-                      color: colorSvg,
-                    ),
-                  )
-                : isBackground && modeDark.value
-                    ? Stack(
-                        alignment: AlignmentDirectional.bottomCenter,
-                        children: [
-                          Container(
-                            width: 45,
-                            height: 45,
-                            decoration: const BoxDecoration(
-                                shape: BoxShape.circle, color: Colors.white),
-                          ),
-                          SvgPicture.asset(
-                            pathSvg!,
-                            color: colorSvg,
-                            height: 50,
-                            width: 50,
-                          ),
-                        ],
-                      )
-                    : SvgPicture.asset(
+    final Uri urls = Uri.parse(url!);
+
+    Future<void> _launchUrl() async {
+      if (!await launch(urls.toString())) {
+        throw 'Could not launch $urls';
+      }
+    }
+
+    return InkWell(
+      onTap: _launchUrl,
+      splashColor: Colors.transparent,
+      highlightColor: Colors.transparent,
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        margin: const EdgeInsets.only(bottom: 10, top: 10),
+        decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.primary,
+            borderRadius: BorderRadius.circular(12)),
+        child:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Row(
+            children: [
+              isContained
+                  ? Container(
+                      padding: const EdgeInsets.all(8),
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                          gradient: isGradientColorContainer
+                              ? LinearGradient(
+                                  begin: Alignment.topRight,
+                                  end: Alignment.bottomLeft,
+                                  stops: const [
+                                      0.1,
+                                      0.4,
+                                      0.6,
+                                      0.9,
+                                    ],
+                                  colors: [
+                                      gradientColorPrimary!,
+                                      gradientColorSecondary!,
+                                      gradientColorTertiary!,
+                                      gradientColorQuaternary!,
+                                    ])
+                              : null,
+                          color:
+                              isGradientColorContainer ? null : colorContainer,
+                          shape: BoxShape.circle),
+                      child: SvgPicture.asset(
                         pathSvg!,
-                        color: isContrast && modeDark.value
-                            ? Colors.white
-                            : colorSvg,
-                        height: 50,
-                        width: 50,
+                        color: colorSvg,
                       ),
-            const SizedBox(
-              width: 20,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  title!,
-                  style: const TextStyle(fontWeight: FontWeight.w500),
-                ),
-                Text(
-                  subTitle!,
-                  style: const TextStyle(fontSize: 14),
-                )
-              ],
-            ),
-          ],
-        ),
-        const Icon(
-          Icons.arrow_forward_ios_rounded,
-        )
-      ]),
+                    )
+                  : isBackground && modeDark.value
+                      ? Stack(
+                          alignment: AlignmentDirectional.bottomCenter,
+                          children: [
+                            Container(
+                              width: 45,
+                              height: 45,
+                              decoration: const BoxDecoration(
+                                  shape: BoxShape.circle, color: Colors.white),
+                            ),
+                            SvgPicture.asset(
+                              pathSvg!,
+                              color: colorSvg,
+                              height: 50,
+                              width: 50,
+                            ),
+                          ],
+                        )
+                      : SvgPicture.asset(
+                          pathSvg!,
+                          color: isContrast && modeDark.value
+                              ? Colors.white
+                              : colorSvg,
+                          height: 50,
+                          width: 50,
+                        ),
+              SizedBox(
+                // width: 20,
+                width: MediaQuery.of(context).size.width *
+                    contactTileSizedBoxWidth,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title!,
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: MediaQuery.of(context).size.height *
+                            contactTileFontSizeTitle),
+                  ),
+                  Text(
+                    subTitle!,
+                    style: TextStyle(
+                        fontSize: MediaQuery.of(context).size.height *
+                            contactTileFontSizeSubtitle),
+                  )
+                ],
+              ),
+            ],
+          ),
+          const Icon(
+            Icons.arrow_forward_ios_rounded,
+          )
+        ]),
+      ),
     );
   }
 }
